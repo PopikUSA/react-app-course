d076958d25aa69efbf30e14d7c5a515a764edf7e
PopikUSA <leonidkamyshnikov@gmail.com>
2025-11-22 13:34:09 +0300

    sort logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState ,useEffect, useRef} from "react"
+import { useState ,useEffect, useMemo} from "react"
 
 import {API_URL} from '../../constants'
 import { QuestionCardList } from "../../components/QuestionCardList"
@@ -10,6 +10,7 @@ import { SearchInput } from "../../components/SearchInput/SearchInput"
 export const HomePage=()=>{
   const [questions, setQuestions]=useState([])
   const [searchValue, setSearchValue]=useState("")
+  const [sortSelectValue, setSortSelectValue]=useState("")
 
 
   const [getQuestions,isLoading,error]=useFetch(async(url)=>{
@@ -19,23 +20,44 @@ export const HomePage=()=>{
     setQuestions(questions)
     return questions
   })
+
+  const cards=useMemo(()=>{
+    return questions.filter((d)=>d.question.toLowerCase().includes(searchValue.trim().toLowerCase()))
+  }, [questions, searchValue])
+
   useEffect(()=>{
-    getQuestions("react")
-  },[])
+    getQuestions(`react?${sortSelectValue}`)
+  },[sortSelectValue])
 
   const onSearchChangeHandler=(e)=>{
     setSearchValue(e.target.value)
+  }
+
+  const onSortSelectChangeHandler=(e)=>{
+    setSortSelectValue(e.target.value)
   }
 
     return <>
 
     <div className={cls.controlsContainer}>
     <SearchInput value={searchValue} onChange={onSearchChangeHandler}/>
+
+    <select value={sortSelectValue} onChange={onSortSelectChangeHandler} className={cls.select}>
+      <option value="">sort by</option>
+      <hr />
+      <option value="_sort=level">level ASC</option>
+      <option value="_sort=-level">level DESC</option>
+      <option value="_sort=completed">Completed ASC</option>
+      <option value="_sort=-completed">Completed  DESC</option>
+    </select>
     </div>
 
        {isLoading && <Loader/>}
        {error && <p>{error}</p>}
-        <QuestionCardList cards={questions}/>
+       {cards.length===0 && <p className={cls.noCardsInfo}>No cards...</p>}
+
+
+        <QuestionCardList cards={cards}/>
 
     </>
 }
