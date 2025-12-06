3474259f175fd0576e8399a60d795969ca5cde5d
PopikUSA <leonidkamyshnikov@gmail.com>
2025-12-06 13:45:50 +0300

    namecnahge i index to default bug fix

--- src/pages/AddQuestionPage/index.jsx
+++ src/pages/AddQuestionPage/index.jsx
@@ -1,3 +1,3 @@
-export { deafult as AddQuestionPage} from "./AddQuestionPage"
+export { default as AddQuestionPage} from "./AddQuestionPage"
 
 export { default as AddQuestionPageLazy} from "./AddQuestionPage.lazy"
